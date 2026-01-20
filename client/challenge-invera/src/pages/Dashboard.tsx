590c30be6569ad8a6b8dbd825dd6fd393faa1262
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2026-01-20 12:16:43 -0300

    Update Dashboard.tsx

--- client/challenge-invera/src/pages/Dashboard.tsx
+++ client/challenge-invera/src/pages/Dashboard.tsx
@@ -1,3 +1,19 @@
+import { PageHeader } from "@/components/PageHeader";
+import { StatsGrid } from "@/components/StatsGrid";
+
 export const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const handleAddNewUser = () => {
+    console.log("handleAddNewUser()");
+  };
+  return (
+    <div className="space-y-6 p-6">
+      <PageHeader
+        title="Users"
+        btnTitle="Add User"
+        handleClick={handleAddNewUser}
+      />
+
+      <StatsGrid />
+    </div>
+  );
 };
