18df85623138c056415632813d447574bec22f4d
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2026-01-21 18:08:19 -0300

    Update UserTableFilters.tsx

--- client/challenge-invera/src/components/custom/UserTableFilters.tsx
+++ client/challenge-invera/src/components/custom/UserTableFilters.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Input } from "../../components/ui/input";
 import { setSearch } from "../../store/userTable/userTableSlice";
@@ -6,6 +7,17 @@ import type { RootState } from "../../store/store";
 export const UsersTableFilters = () => {
   const dispatch = useDispatch();
   const { search } = useSelector((state: RootState) => state.usersTable);
+  const [localSearch, setLocalSearch] = useState(search);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(setSearch(localSearch));
+    }, 500);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [localSearch, dispatch]);
 
   return (
     <div className="flex items-center gap-4 w-full flex-wrap">
@@ -13,8 +25,8 @@ export const UsersTableFilters = () => {
 
       <Input
         placeholder="Search for..."
-        value={search}
-        onChange={(e) => dispatch(setSearch(e.target.value))}
+        value={localSearch}
+        onChange={(e) => setLocalSearch(e.target.value)}
         className="max-w-sm"
       />
     </div>
