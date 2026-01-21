61567a65044ca298bde4d728e7581ee9e875581d
PicassoOctavio <54865779+PicassoOctavio@users.noreply.github.com>
2026-01-21 09:43:05 -0300

    Update useUsersStatics.ts

--- client/challenge-invera/src/hooks/useUsersStatics.ts
+++ client/challenge-invera/src/hooks/useUsersStatics.ts
@@ -1,8 +1,9 @@
+import type { UsersStaticsI } from "@/types/usersStatics.response";
 import { getUsersStatics } from "@/actions/userStatics.action";
 import { useQuery } from "@tanstack/react-query";
 
 export const useUsersStatics = () => {
-  return useQuery({
+  return useQuery<UsersStaticsI>({
     queryKey: ["usersStatics"],
     queryFn: getUsersStatics,
   });
